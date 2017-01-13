16c70a6299c2f6c6c7450d0c688f83e74cc58817
dave <dave@agileinf.com>
2017-01-13 23:10:35 +0530

    notification working debug mode

--- models/user.js
+++ models/user.js
@@ -3,13 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    full_name: String,
+    full_name: {type:String, required: true},
     email: {type:String, required:true, index:{unique:true}},
-    password: {type: String, required: true, select: false},
+    password: {type: String, required: true},
     contact_number: {type:String, required: true},
+    full_address:{type:String, required: true},
     state: {type:Number, required: true, default:2},
-    last_login: {type:Date, default:null},
+    last_login: {type:Date, default:Date.now()},
     date_of_signup: {type:Date, default:Date.now()},
-    last_updated: {type:Date, default:Date.now()}
+    last_updated: {type:Date, default:Date.now()},
+    device_id:{type:String}
 });
 module.exports = mongoose.model('User', userSchema);
