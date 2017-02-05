d496b475815736448f9a4dad35f29195fba103ed
dave <dave@agileinf.com>
2017-02-05 23:51:56 +0530

    cert and key regenerated

--- APIControllers/NotificationAPIController.js
+++ APIControllers/NotificationAPIController.js
@@ -49,7 +49,7 @@ module.exports = {
 		      			let tokens = [device_id];
 				    	var options = {
 						  key:"key.pem",
-						  production: false,
+						  production: true,
 						};
 		      			var apnProvider = new apn.Provider(options);
 		      			var alert = placeCountforNotification + " Place(s) within 5KM of your location";
@@ -80,77 +80,4 @@ module.exports = {
     	    	
     }
     
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    	/*var query = User.findOne({ '_id': req.body._id });
-        query.exec(function(err,user){
-            Adventure.aggregate(
-	              [
-	                { "$geoNear": {
-	                    "near": {
-	                      "type": "Point",
-	                      "coordinates": [ req.body.location.lon, req.body.location.lat ]
-	                    },
-	                    "maxDistance": 5000,
-	                    "spherical": true,
-	                    "distanceField": "dis",
-	                    "distanceMultiplier":1/1000,
-	                    "query":{},
-	                }},
-	                { "$sort": { "dis": 1 } } 
-
-	              ],function(err,adventures){
-	              		var noOfAdventures = adventures.length;
-	                    if (noOfAdventures) {
-	                    	for (var i = 0; i < noOfAdventures; i++){
-	                    		var query = User.findOne({})
-	                    		/*if (q.length == 0) {
-	                    			User.findOneAndUpdate({_id: user._id}, {$push:{notifications:{place_id:adventures[i]._id}}}, {new: true}, function(err, doc){
-									    if(err){
-									        console.log("Something wrong when updating data!");
-									    }
-									    //console.log(doc);
-									});
-
-	                    		}
-	                    		
-	                    	}
-	                    }
-	              }
-	        ); 
-
-        })	
-
-		var apnProvider = new apn.Provider(options);
-
-		let note = new apn.Notification({
-			alert:  "Place 1 is within 5KM of your location",
-		});
-		note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-		note.badge = 3;
-		note.sound = "ping.aiff";
-		note.topic = "com.ai.ponoadventure";
-
-		apnProvider.send(note, tokens).then( result => {
-			console.log(result);
-		    console.log("sent:", result.sent.length);
-		    console.log("failed:", result.failed.length);
-		    console.log(result.failed);
-		});
-		res.json(req.body);
-		apnProvider.shutdown();*/
+}	
