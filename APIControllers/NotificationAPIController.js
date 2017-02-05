d61b4c44160c8198aa79d3b9be81b26879870d0d
dave <dave@agileinf.com>
2017-02-05 23:07:20 +0530

    cert to key mistake

--- APIControllers/NotificationAPIController.js
+++ APIControllers/NotificationAPIController.js
@@ -48,7 +48,7 @@ module.exports = {
 	      			if (placeCountforNotification != 0) {
 		      			let tokens = [device_id];
 				    	var options = {
-						  key:"cert.pem",
+						  key:"key.pem",
 						  production: false,
 						};
 		      			var apnProvider = new apn.Provider(options);
