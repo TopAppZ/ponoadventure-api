6e6bfdc551a14e62d3ce01c6fe3a55eccb78a118
daveai <dave@agileinf.com>
2017-02-26 21:10:26 +0530

    login added from as basic authentication

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var morgan = require("morgan");
 var config = require('./config');
 var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 var app = express();
 mongoose.connect(config.database, {auth:{authdb:"admin"}}, function(err){
     if(err){
@@ -14,6 +18,8 @@ mongoose.connect(config.database, {auth:{authdb:"admin"}}, function(err){
 app.use(bodyParser.json({limit: '50MB'}));
 app.use(bodyParser.urlencoded({limit: '50MB', extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({secret: "Shh, its a secret!", resave:true, saveUninitialized:true, path: '/' }));
 
 var router = require('./router')(express, app);
 app.listen(config.port, function(){
--- public/controllers/admin/NavigationController.js
+++ public/controllers/admin/NavigationController.js
@@ -1,10 +1,10 @@
-app.controller('NavigationController',['$scope','SharedDataService','CategoryService',function($scope, sharedDataService, categoryService){      
-	var categories = categoryService.query(function() {       
+app.controller('NavigationController',['$scope','SharedDataService','CategoryService',function($scope, sharedDataService, categoryService){
+	var categories = categoryService.query(function() {
         $scope.categories = categories;
-        sharedDataService.categories = categories;        
+        sharedDataService.categories = categories;
         $("#progressWrapper").hide();
     });
-    $scope.$watch(function () { return sharedDataService.categories }, function (newVal, oldVal) {        
+    $scope.$watch(function () { return sharedDataService.categories }, function (newVal, oldVal) {
         $scope.categories = sharedDataService.categories;
-    });
+    });		
 }]);
